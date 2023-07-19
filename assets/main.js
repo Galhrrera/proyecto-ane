f5b8c5d99fbe161a6e4213e496feefdae9282595
galhrrera <juan.galeanoherrera@gmail.com>
2023-07-18 21:29:34 -0500

    Menu responsive works

--- assets/main.js
+++ assets/main.js
@@ -12,3 +12,11 @@
 //     body.classList.toggle("body-moved")
 //     menu_container.classList.toggle("menu-container-moved")
 // }
+var body = document.getElementById("my-body")
+var menu_container = document.getElementById("menu-container")
+window.addEventListener("resize", function(){
+    if (this.window.innerWidth > 760){
+        body.classList.remove("body-moved")
+        menu_container.classList.remove("menu-container-moved")
+    }
+})
